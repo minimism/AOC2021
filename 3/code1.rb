aae899aa2f01cfd06ca772d6bb9b278993c92787
Tuffen John (ETAS/ESW-MW) <john.tuffen@etas.com>
2021-12-03 11:57:43 +0000

    A small refactor

--- 3/code1.rb
+++ 3/code1.rb
@@ -2,27 +2,27 @@
 if (ARGV.count != 1)
     puts "Please supply a filename"
 else
-    lines = 0
-    lineLength = 0
-    results = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-    File.open(ARGV[0]).each_with_index do |line, linenumber|
-        line.strip().split('').reverse.each_with_index do |c,idx|
+    lines = File.read(ARGV[0]).split("\n")
+    lineCount = lines.length()
+    lineLength = lines[0].length()  # assume all lines are equal length
+    results = Array.new(lineLength,0)
+
+    lines.each do |line|
+        line.split('').reverse.each_with_index do |c,idx|
             results[idx] = results[idx] + c.to_i
-            lineLength = idx
         end
-        lines = linenumber
     end
-    p lines
+    p lineCount
     gamma = 0
-    results[0..lineLength].each_with_index do |count, idx|
-        if (count > (lines/2))
+    results[0...lineLength].each_with_index do |count, idx|
+        if (count > (lineCount/2))
             puts "most common for bit #{idx} is 1"
             gamma = gamma + (1 << (idx))
         else
             puts "most common for bit #{idx} is 0"
         end
     end
-    epsilon = (1 << (lineLength + 1)) - gamma - 1
+    epsilon = (1 << lineLength) - gamma - 1
     puts "gamma = #{gamma}"
     puts "epsilon = #{epsilon}"
     puts "answer = #{gamma * epsilon}"
